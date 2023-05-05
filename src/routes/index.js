052dea08bbacfcf14f40c33c1b1efd74ca634bbb
Axel José Mosquito <112713383+AxlRoose1702@users.noreply.github.com>
2023-05-05 10:25:09 -0600

    Se configura swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
 require('express-async-errors');
+const swaggerUI = require('swagger-ui-express');
+const {SWAGGER_PATH} = require('../config');
+const swaggerDocument = require(SWAGGER_PATH);
 
 module.exports = function({
     homeRoutes, 
@@ -29,6 +32,7 @@ module.exports = function({
     apiRoutes.use("/auth", authRoutes);
 
     router.use('/v1/api', apiRoutes);
+    router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
     //se agrega los middleware para mostrar paginas de errores
     router.use(NotfoundMiddleware);
